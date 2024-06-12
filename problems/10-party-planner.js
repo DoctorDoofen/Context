3527753abc891919d4fb55de30f6e36428d7797a
esketchandu <esketchandu10@gmail.com>
2024-06-11 22:00:52 -0700

    party planner

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,4 +1,25 @@
-// Your code here 
+class PartyPlanner{
+	constructor(guestlist){
+		this.guestlist = [];
+	}
+
+	addToGuestList(name){
+		this.guestlist.push(name);
+	}
+
+	throwParty(){
+		if(this.guestlist.length === 0) {
+			return "Gotta add people to the guest list"
+		}
+		else {
+			for(let guest of this.guestlist) {
+				return `Welcome to the party ${guest}`
+			}
+
+		}
+
+	}
+}
 
 
 /*****************************************************************************/
